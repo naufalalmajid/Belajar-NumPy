696ea24071e7dd98eedbe6605804561f0f92030d
naufal al majid <70688082+naufalalmajid@users.noreply.github.com>
2024-04-02 20:05:59 +0700

    Create arrayshape.py

---
+++ arrayshape.py
@@ -0,0 +1,6 @@
+# the shape of an array it is mean the number of elements in each dimension
+# we can get the shape of an array by using shape attribute
+import numpy as np
+
+three = np.array([[[2, 22], [8, 88]], [[28, 88], [222, 888]]])
+print(three.shape)
