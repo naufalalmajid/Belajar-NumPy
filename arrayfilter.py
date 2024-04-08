046722e4bc0e3042fa44f6c5e343b9bb0283d916
naufal al majid <70688082+naufalalmajid@users.noreply.github.com>
2024-04-08 16:54:55 +0700

    Update arrayfilter.py

--- arrayfilter.py
+++ arrayfilter.py
@@ -1,1 +1,30 @@
+# getting some elements out of an existing array and creating a new array out(filtering)
+# in numpy we can filter an array using a boolean index list
+import numpy as np
 
+yearOld = np.array([2002, 2024, 21])
+x = [True, False, True]
+newArr = yearOld[x]
+print(newArr)
+
+# creating the filter array
+arr = np.array([2, 8, 28, 99, 222, 555, 2222])
+filterArr = []
+
+for value in arr:
+    if value >= 28:
+        filterArr.append(True)
+    else:
+        filterArr.append(False)
+
+
+newValue = arr[filterArr]
+print(filterArr)
+print(newValue)
+
+# creating filter directly from array
+diretctArr = arr >= 28
+newArrdirect = arr[diretctArr]
+
+print(diretctArr)
+print(newArrdirect)
