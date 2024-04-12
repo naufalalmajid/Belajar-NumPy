5a3835e6867a8920d3585e7a2c655b7b8bd58520
naufal al majid <70688082+naufalalmajid@users.noreply.github.com>
2024-04-12 23:04:14 +0700

    Update binomialdistribution.py

--- binomialdistribution.py
+++ binomialdistribution.py
@@ -1,1 +1,19 @@
+# binomial distribution is a discrete distribution which it is defined as separate set of events e.g. a coin toss's result
+# n number of trials
+# p probability of occurence of each trial
+# size shape of the returned array
+from numpy import random
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+x = random.binomial(n=10, p=0.5, size=10)
+print(x)
+
+# visualization of binomial distribution and difference with normal distribution
+sns.distplot(
+    random.normal(loc=50, scale=5, size=1000), hist=False, label="Normal Distribution"
+)
+sns.distplot(
+    random.binomial(n=100, p=0.5, size=1000), hist=False, label="Binomial Distribution"
+)
+plt.show()
