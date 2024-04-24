c3cc7cdc5079c716b72feec93de9297e91bd48fb
naufal al majid <70688082+naufalalmajid@users.noreply.github.com>
2024-04-24 22:48:24 +0700

    Update multinomialdistribution.py

--- multinomialdistribution.py
+++ multinomialdistribution.py
@@ -1,1 +1,8 @@
+# multinomial distribution is a generelization of binomial distribution
+# n number of possible outcomes
+# pvals list of probabilities
+# size the shape of returned array
+from numpy import random
 
+coin = random.multinomial(n=1, pvals=[4 / 5, 1 / 5])
+print(coin)
